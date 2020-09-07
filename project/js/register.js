f5eb218e49ef41c1378902b49f61ab9d976255e6
yeongil4764 <ho1325646@gmail.com>
2020-09-07 18:24:07 +0900

    09071800 회원가입

--- project/js/register.js
+++ project/js/register.js
@@ -3,21 +3,98 @@ $(function () {
     $('#cfm-password').keyup(function () {
 
         var pwd = $("#password").val();
-        var cpwd = $("input:focus").val();
+        var cpwd = $("#cfm-password").val();
 
-        if (pwd == cpwd) {
-            $(".btn").attr({
-                "color":"red"
-            });
-            alert("성공");
-        } else {
-            $(".btn").attr({
-                "color":"#fff"
-            });
-            alert("실패");
+        if (pwd != null && pwd != '') {
+            if (pwd == cpwd) {
+                $("#pwdcheck").addClass("green");
+                $('#btn').removeClass("disen");
+                $('#btn').prop('disabled', false);
+
+            } else {
+                $("#pwdcheck").removeClass("green");
+                $("#pwdcheck").addClass("red");
+                $('#btn').addClass("disen");
+                $('#btn').prop('disabled', true);
+
+            }
         }
     });
 
-    
+    $('#password').keyup(function () {
 
+        var pwd = $("#password").val();
+        var cpwd = $("#cfm-password").val();
+
+        if (pwd != null && pwd != '') {
+            if (pwd == cpwd) {
+                $("#pwdcheck").addClass("green");
+                $('#btn').removeClass("disen");
+                $('#btn').attr({
+                    "disabled": "false"
+                });
+
+            } else {
+                $("#pwdcheck").removeClass("green");
+                $("#pwdcheck").addClass("red");
+                $('#btn').addClass("disen");
+                $('#btn').attr({
+                    "disabled": "disabled"
+                });
+
+            }
+        }
+    });
+
+
+    $(".pos #phone").keyup(function () {
+        var RegExp = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi;//정규식 구문
+        var obj = $("#phone").val();
+        if (RegExp.test(obj)) {
+            alert("특수문자는 입력하실 수 없습니다.");
+            $(".pos #phone").val(obj.slice(0, -1));
+        }
+
+    });
+
+    $('#btn').click(function () {
+        var id = $("#userid").val;
+        var name = $("#name").val;
+        var pwd = $("#password").val;
+        var cnt = 0;
+
+        alert(id + name + pwd);
+
+        // if (id == '' || id == null) {
+        //     alert("사번을 입력해주세요.");
+        //     return false;
+
+        // }
+        // else {
+        //     cnt++;
+        // }
+
+        // if (name == '' || name == null) {
+        //     alert("사번을 입력해주세요.");
+        //     return false;
+        // } else {
+        //     cnt++;
+        // }
+
+        // if (pwd == '' || pwd == null) {
+        //     alert("사번을 입력해주세요.");
+        //     return false;
+        // } else {
+        //     cnt++;
+        // }
+
+        // if (cnt = 3) {
+        //     location.href = "./SFA_login.html";
+        // }
+
+    });
 });
+
+
+
+
